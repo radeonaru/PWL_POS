d31ba2ec7109b4282fa3c48b24e1cd5b33268b33
Indra <radeonaru@gmail.com>
2024-03-09 21:08:35 +0700

    chore: trying fillable property on UserModel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,17 +11,19 @@ class UserController extends Controller
     public function index()
     {
         // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
         // ];
-        // UserModel::insert($data);
 
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
         
         $user = UserModel::all();
         return view('user', ['data' => $user]);
