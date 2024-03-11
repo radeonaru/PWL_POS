a3b4014b9f7809800fd586ab1e48f07456a1937f
Indra <radeonaru@gmail.com>
2024-03-11 13:40:56 +0700

    chore: retrieving single models from UserModel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,15 +17,24 @@ class UserController extends Controller
         //     'password' => Hash::make('12345')
         // ];
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // $user = UserModel::where('level_id', 1);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
