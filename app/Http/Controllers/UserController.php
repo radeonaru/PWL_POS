8e3e53fba770b943ad3e64c14baa0b3a7fec7abc
Indra <radeonaru@gmail.com>
2024-03-19 20:15:37 +0700

    chore: trying metode findOrFail and firstOrFail from UserModel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,9 +43,15 @@ class UserController extends Controller
 
         // $user = UserModel::where('level_id', 1);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
+
+
     }
 }
