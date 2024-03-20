622bd8c4b0b3b446ea1496b88315a196cac59134
Indra <radeonaru@gmail.com>
2024-03-20 11:38:08 +0700

    feat: add relationship on databases

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -116,8 +116,11 @@ class UserController extends Controller
         // $user->wasChanged('nama');
         // dd($user->wasChanged(['nama', 'username']));
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
+        dd($user);
     }
 
     public function tambah()
@@ -164,4 +167,6 @@ class UserController extends Controller
 
         return redirect('/user');
     }
+
+
 }
