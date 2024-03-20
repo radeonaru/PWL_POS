7cdb80ae5b08a674716b46d92d0b243e0163b40d
Indra <radeonaru@gmail.com>
2024-03-20 08:09:08 +0700

    chore: Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,10 +51,28 @@ class UserController extends Controller
 
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         
-        $user = UserModel::where('level_id', 2)->count();
+        // $user = UserModel::where('level_id', 2)->count();
         // dd($user);
+        // return view('user', ['data' => $user]);
+        
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
-
-
     }
 }
